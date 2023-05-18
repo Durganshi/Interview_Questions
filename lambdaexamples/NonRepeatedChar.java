bbee81ac7b503be2d95462f6f1219f892974aee4
Durganshi <durganshichoudhary8179@gmail.com>
2023-05-18 14:52:56 +0530

    first upload

---
+++ lambdaexamples/NonRepeatedChar.java
@@ -0,0 +1,17 @@
+package lambdaexamples;
+
+import java.util.List;
+import java.util.Optional;
+
+public class NonRepeatedChar {
+    public static void main(String[] args) {
+
+        String str = "mayanksingh";
+        Optional<Character> nonRepeatedChar = str.chars()
+                .mapToObj(ch -> (char) ch)
+                .filter(ch -> str.indexOf(ch) == str.lastIndexOf(ch))
+                .findAny();
+        nonRepeatedChar.ifPresent(System.out::println);
+
+    }
+}
